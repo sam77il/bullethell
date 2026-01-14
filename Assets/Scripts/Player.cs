b1ea29fe53f65ee4d747efd76a7b2266df2a35f2
Furkan Tuna <furkan.07tuna@gmail.com>
2026-01-14 15:57:43 +0100

    WASD and Smooth Camera

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    public float maxSpeed = 5f;
+    public float acceleration = 20f;
+    public float deceleration = 25f;
+
+    private Vector2 moveInput;
+    private Vector2 currentVelocity;
+
+    // PlayerInput → Invoke Unity Events → Player/Move
+    public void Move(InputAction.CallbackContext context)
     {
-        
+        moveInput = context.ReadValue<Vector2>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Zielgeschwindigkeit
+        Vector2 targetVelocity = moveInput * maxSpeed;
+
+        // Beschleunigen oder Abbremsen
+        float accelRate = (moveInput.sqrMagnitude > 0.01f)
+            ? acceleration
+            : deceleration;
+
+        currentVelocity = Vector2.MoveTowards(
+            currentVelocity,
+            targetVelocity,
+            accelRate * Time.deltaTime
+        );
+
+        transform.Translate(currentVelocity * Time.deltaTime);
     }
 }
